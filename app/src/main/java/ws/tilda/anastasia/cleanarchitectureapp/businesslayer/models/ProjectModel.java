2e9d09bf5908bdbada1740694bbae3d50363eb83
Anastasia Karpenko <morganaloca@gmail.com>
2018-11-06 18:01:28 +0200

    Adding 3 packages inside business layer package, adding ProjectModel class

---
+++ app/src/main/java/ws/tilda/anastasia/cleanarchitectureapp/businesslayer/models/ProjectModel.java
@@ -0,0 +1,22 @@
+package ws.tilda.anastasia.cleanarchitectureapp.businesslayer.models;
+
+public class ProjectModel {
+    String mTitle;
+    String mDescription;
+
+    public String getTitle() {
+        return mTitle;
+    }
+
+    public void setTitle(String title) {
+        mTitle = title;
+    }
+
+    public String getDescription() {
+        return mDescription;
+    }
+
+    public void setDescription(String description) {
+        mDescription = description;
+    }
+}
