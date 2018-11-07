6b6588bfd7ca8da65d0bbd4c450cbf140f081ebf
Anastasia Karpenko <morganaloca@gmail.com>
2018-11-07 17:45:57 +0200

    Make MainActivity implement the interface of ProjectPresenter.View

--- app/src/main/java/ws/tilda/anastasia/cleanarchitectureapp/presentationlayer/ui/activities/MainActivity.java
+++ app/src/main/java/ws/tilda/anastasia/cleanarchitectureapp/presentationlayer/ui/activities/MainActivity.java
@@ -3,13 +3,37 @@ package ws.tilda.anastasia.cleanarchitectureapp.presentationlayer.ui.activities;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
 
-import ws.tilda.anastasia.cleanarchitectureapp.R;
+import java.util.List;
 
-public class MainActivity extends AppCompatActivity {
+import ws.tilda.anastasia.cleanarchitectureapp.R;
+import ws.tilda.anastasia.cleanarchitectureapp.businesslayer.models.ProjectModel;
+import ws.tilda.anastasia.cleanarchitectureapp.presentationlayer.presenter.ProjectPresenter;
+
+public class MainActivity extends AppCompatActivity implements ProjectPresenter.View {
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+    }
+
+    @Override
+    public void displayProjectList(List<ProjectModel> projectModelList) {
+
+    }
+
+    @Override
+    public void showProgress() {
+
+    }
+
+    @Override
+    public void hideProgress() {
+
+    }
+
+    @Override
+    public void showError(String error) {
+
     }
 }
